323886414139d9a5dad81e08a729e8f2dccd2a72
kr-zephyr <scin21c@gmail.com>
2017-01-11 18:21:32 +0900

    기본적인 람다식 예제

---
+++ src/testcodes/PracticeLambda.java
@@ -0,0 +1,12 @@
+public class PracticeLambda {
+
+    public static void main(String[] args) {
+        Func add = (int a, int b) -> a + b;
+        System.out.println("sum 2, 3 :: " + add.add(2, 3));
+    }
+}
+
+@FunctionalInterface
+interface Func {
+    int add(int x, int y);
+}
