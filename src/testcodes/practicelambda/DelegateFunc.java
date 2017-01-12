5944b0f573946cc3befe6057eb7322a838ac24f9
kr-zephyr <scin21c@gmail.com>
2017-01-12 12:22:33 +0900

    java8 Lambda의 func delegate 기본

---
+++ src/testcodes/practicelambda/DelegateFunc.java
@@ -0,0 +1,25 @@
+package practicelambda;
+
+public class DelegateFunc {
+
+    public static void main(String[] args) {
+        // Function 정의
+        CountFunc cCount = (char[] c) -> c.length;
+
+        System.out.println("도메인의 캐릭터 수 :: " + countDomain(cCount));
+        System.out.println("특정 문자열의 캐릭터 수 :: " + countHello(cCount));
+    }
+
+    static int countDomain(CountFunc countFunc) {
+        return countFunc.count("http://www.google.com".toCharArray());
+    }
+
+    static int countHello(CountFunc countFunc) {
+        return countFunc.count("Hello world!".toCharArray());
+    }
+}
+
+@FunctionalInterface
+interface CountFunc {
+    int count(char[] c);
+}
