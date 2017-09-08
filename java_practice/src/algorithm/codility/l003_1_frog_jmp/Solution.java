6ce98f7221c40d9c283fd482fa898dc633366823
kr-zephyr <scin21c@gmail.com>
2017-09-08 18:40:31 +0900

    codility FrogJmp 100점
    codility FrogJmp 100점
    Double.ceil은 반드시 파라메터에 double이 있어야 정상적으로 ceil됨.

--- java_practice/src/algorithm/codility/l003_1_frog_jmp/Solution.java
+++ java_practice/src/algorithm/codility/l003_1_frog_jmp/Solution.java
@@ -2,12 +2,15 @@ package algorithm.codility.l003_1_frog_jmp;
 
 // you can also use imports, for example:
 // import java.util.*;
+import java.lang.Math;
 
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
 class Solution {
-    public int solution(int X, int Y, int D) {
-        return (((Y / D) * D) + X) >= Y ? ((Y - X) / D) : ((Y - X) / D) + 1;
-    }
+	public int solution(int X, int Y, int D) {
+		// System.out.println(((Y - X) / D) + ", " + Math.ceil((Y - X) / (double) D));
+		int jmpCnt = (int) Math.ceil((Y - X) / (double) D);
+		return jmpCnt < 0 ? 0 : jmpCnt;
+	}
 }
